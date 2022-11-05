e22bd8e7f5b4d93851ec69a20e84c952aa2708d4
Felix Chippendale <felix.chippendale@gmail.com>
2022-11-05 10:42:58 +0000

    fix inconsistent hashing

--- validation/steganography_tests.py
+++ validation/steganography_tests.py
@@ -1,11 +1,13 @@
+import hashlib
+
 def validate(function):
-    EXP_OUTPUT = 4232911600771056735
+    EXP_OUTPUT = "a6d72baa3db900b03e70df880e503e9164013b4d9a470853edc115776323a098"
     
     print(f"Running Testcase")
     func_output = function()
     assert isinstance(func_output, str), "Function did not return a string"
 
-    if hash(func_output) == EXP_OUTPUT:
+    if hashlib.sha256(func_output.encode("utf-8")).hexdigest() == EXP_OUTPUT:
         print("Test Case Passed")
     else:
         print('Test Case Failed')
